6df48a5e5a55aae8dfbea5237ea35aec11767b3a
larinatuktabaeva44364-dotcom <larinatuktabaeva44364@gmail.com>
2025-10-05 13:03:25 +0200

    Create operazione1.py

---
+++ operazione1.py
@@ -0,0 +1,6 @@
+a=float(input("Quandi euro hai?"))
+b=float(input("Scrivi il prezzo di un singolo ogetto?"))
+oggeti=int(a//b)
+resto=a%b
+print("Poi comprare", oggeti,"oggeti")
+print("Hai resto",resto,"resto")
